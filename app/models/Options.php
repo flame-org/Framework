453ca03a3c74cfc94d60496c0329594b8b9da8b5
JSifalda <sifalda.jiri@gmail.com>
2012-06-24 09:07:34 +0200

    added global settings

---
+++ app/models/Options.php
@@ -0,0 +1,32 @@
+<?php
+
+use Nette\Database\Connection, Nette\Database\Table\Selection;
+
+/**
+* Global variables and settings
+*/
+class OptionsModel extends Selection
+{
+	
+	public function __construct(Connection $c)
+	{
+		parent::__construct('option', $c);
+	}
+
+	public function getAll()
+	{
+		return $this->order('id DESC');
+	}
+
+	public function getByName($name)
+	{
+		$var = $this->where(array('name' => $name))->fetch();
+
+		if($var === false){
+			return null;
+		}else{
+			return $var->value;
+		}
+	}
+}
+?>
