8a8e500bcaff8f5850391f45fcf0043153f94838
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-11 17:22:20 +0200

    comments component for admin module removed

--- app/AdminModule/presenters/DashboardPresenter.php
+++ app/AdminModule/presenters/DashboardPresenter.php
@@ -8,24 +8,8 @@ namespace AdminModule;
 class DashboardPresenter extends AdminPresenter
 {
 
-	private $service;
-	private $unPublishComments;
-
 	public function actionDefault()
 	{
-		$this->service = $this->context->comments;
-		$this->unPublishComments = $this->service->findBy(array('publish' => '0'));
-		$this->template->unPublishComments = $this->unPublishComments;
-	}	
-
-	protected function createComponentCommentsControl()
-	{
-		if(!$this->unPublishComments){
-			return null;
-		}else{
-			return new Components\CommentsControl($this->unPublishComments, $this->service);
-		}
 	}
-
 }
 ?>
