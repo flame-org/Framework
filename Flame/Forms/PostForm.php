c930f7e636eb14288e1127d1b1f585f88221eb1b
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-14 16:24:44 +0200

    added default form for post

---
+++ Flame/Forms/PostForm.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * PostForm.php
+ *
+ * @author  Jiří Šifalda <jsifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    14.07.12
+ */
+
+namespace Flame\Forms;
+
+class PostForm extends \Nette\Forms\Form
+{
+
+	public function configureAdd()
+	{
+		$this->configure();
+		$this->addCheckbox('comment', 'Allow comments?')
+			->setDefaultValue('1');
+		$this->addSubmit('create', 'Create post');
+
+	}
+
+	public function configureEdit(array $defaults)
+	{
+		$this->configure();
+		$this->setDefaults($defaults);
+		$this->addCheckbox('comment', 'Allow comments?');
+		$this->addSubmit('edit', 'Edit post');
+
+	}
+
+	private function configure()
+	{
+		$this->addText('name', 'Name:', 80)
+			->addRule(self::FILLED, 'Name is required.')
+			->addRule(self::MAX_LENGTH, 'Name of post must be shorter than %d chars', 100);
+
+		$this->addText('slug', 'Name in URL', 80)
+			->addRule(self::MAX_LENGTH, 'Name in URL must be shorter than %d chars', 100);
+
+		$this->addText('keywords', 'META Keywords', 80)
+			->addRule(self::MAX_LENGTH, 'Meta keywords must be shorter than %d chars', 500);
+
+		$this->addTextArea('description', 'Descriptions', 90, 5)
+			->addRule(self::MAX_LENGTH, 'Descriptions must be shorter than %d chars', 250);
+
+		$this->addTextArea('content', 'Content:', 115, 35)
+			->addRule(self::FILLED, 'Content is required.')
+			->getControlPrototype()->class('mceEditor');
+
+		$this->addCheckbox('publish', 'Make this post published?');
+
+		$this->onSuccess[] = callback($this->getPresenter(), 'postFormSubmitted');
+	}
+}
