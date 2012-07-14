d446ee52102ce8ad268335dba1fbcd8a2ac5c45f
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-14 19:03:08 +0200

    added default messages for forms fules

--- Flame/Application/UI/Presenter.php
+++ Flame/Application/UI/Presenter.php
@@ -10,11 +10,15 @@
 
 namespace Flame\Application\UI;
 
+use Nette\Forms\Rules,
+	Flame\Application\UI\Form;
+
 abstract class Presenter extends \Nette\Application\UI\Presenter
 {
 	public function startup()
 	{
 		parent::startup();
+		$this->setDefaultsFormMessages();
 	}
 
 	public function beforeRender()
@@ -36,5 +40,19 @@ abstract class Presenter extends \Nette\Application\UI\Presenter
 			'loader'
 		));
 		return $template;
+	}
+
+	private function setDefaultsFormMessages()
+	{
+		Rules::$defaultMessages = array(
+			Form::EQUAL => 'Please enter %s.',
+			Form::FILLED => 'Field "%label" is required.',
+			Form::MIN_LENGTH => 'Field "%label" must be longer than %d chars.',
+			Form::MAX_LENGTH => 'Field "%label" must be shorter than %d chars.',
+			Form::LENGTH => 'Value of field "%label" must be longer than %d and shorter than %d chars.',
+			Form::EMAIL => 'Field "%label" must be valid email address.',
+			Form::URL => 'Field "%label" must be valid URL address.',
+			Form::IMAGE => 'You can upload only JPEG, GIF or PNG files.'
+		);
 	}
 }
