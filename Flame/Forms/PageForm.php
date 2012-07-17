2700fac5789b16a8c41591781cb9ae818bc6e7fc
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-17 21:23:01 +0200

    added page form

---
+++ Flame/Forms/PageForm.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * PageForm.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    17.07.12
+ */
+
+namespace Flame\Forms;
+
+class PageForm extends \Flame\Application\UI\Form
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	
+	public function configureEdit()
+	{
+		$this->configure();
+		$this->addSubmit('send', 'Edit page');
+	}
+	
+	public function configureAdd()
+	{
+		$this->configure();
+		$this->addSubmit('send', 'Create page');
+	}
+	
+	private function configure()
+	{
+
+		$this->addText('name', 'Name:', 80)
+			->addRule(self::FILLED)
+			->addRule(self::MAX_LENGTH, null, 100);
+
+		$this->addText('slug', 'Slug:', 80);
+
+		$this->addTextArea('description', 'Description:', 80, 6)
+			->addRule(self::MAX_LENGTH, null, 250);
+
+		$this->addTextArea('keywords', 'META keywords:', 80, 6)
+			->addRule(self::MAX_LENGTH, null, 250);
+
+		$this->addTextArea('content', 'Content:', 110, 30)
+			->addRule(self::FILLED)
+			->getControlPrototype()->class('mceEditor');
+	}
+
+}
