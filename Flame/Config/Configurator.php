ce2e4e3240533d7d898ab6a4ba200e07a116d4f8
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-21 14:06:04 +0200

    added configurator

---
+++ Flame/Config/Configurator.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Configurator.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    21.07.12
+ */
+
+namespace Flame\Config;
+
+class Configurator extends \Nette\Config\Configurator
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function setOptionalParameters()
+	{
+		//$baseUrl = rtrim($this->container->httpRequest->getUrl()->getBaseUrl(), '/');
+		$baseUrl = '';
+
+		$this->addParameters(array(
+			'baseUrl' => $baseUrl,
+			'basePath' => preg_replace('#https?://[^/]+#A', '', $baseUrl),
+			'flameDir' => realpath(FLAME_DIR),
+		));
+	}
+
+	public function setDatabaseParametersFromEnv()
+	{
+		if(isset($_SERVER['DB1_HOST'], $_SERVER['DB1_NAME'], $_SERVER['DB1_USER'], $_SERVER['DB1_PASS'])){
+			$this->addParameters(array(
+				'parameters' => array(
+					'database' => array(
+						'host' => $_SERVER['DB1_HOST'],
+						'dbname' => $_SERVER['DB1_NAME'],
+						'user' => $_SERVER['DB1_USER'],
+						'password' => $_SERVER['DB1_PASS'],
+					),
+				),
+			));
+		}
+	}
+
+}
