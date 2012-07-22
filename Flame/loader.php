26fe9b4707fd5d3fd44b3af9e8d661d3153fd929
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-22 13:44:38 +0200

    moved to bootstrap file

--- Flame/loader.php
+++ Flame/loader.php
@@ -1,20 +1,4 @@
 <?php
 
-use Flame\Config\Configurator;
-
 define('FLAME_DIR', __DIR__ );
-
-if(!defined('NETTE')) die ('You must load Nette loader first');
-
-$configurator = new Configurator();
-$configurator->setOptionalParameters();
-$configurator->enableDebugger(WWW_DIR . '/log');
-$configurator->setTempDirectory(WWW_DIR . '/temp');
-$configurator->createRobotLoader()->addDirectory(APP_DIR)->register();
-$configurator->addConfig(FLAME_DIR . '/Config/config.neon');
-$configurator->setDatabaseParametersFromEnv();
-$container = $configurator->createContainer();
-
-$doctrineConfig = $container->getService('EntityManagerConfig');
-$doctrineConfig->setSQLLogger(\Flame\Utils\ConnectionPanel::register());
 
