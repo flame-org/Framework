18018edb3319e89fbbd49625ae87d92ebb859946
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-02 17:41:51 +0200

    added user info

--- Flame/Security/Identity.php
+++ Flame/Security/Identity.php
@@ -18,14 +18,25 @@ class Identity extends \Nette\Security\Identity
 	public function __construct(\Flame\Models\Users\User $user)
 	{
 		$this->user = $user;
-
 		$this->user->setPassword(null);
-		parent::__construct($this->user->getId(), $this->user->getRole(), $this->user->toArray());
+
+		parent::__construct($this->user->getId(),$this->user->getRole(), $this->getUserData());
+
+		$this->user->setInfoNull();
 	}
 
 	public function getUserModel()
 	{
 		return $this->user;
+	}
+
+	private function getUserData()
+	{
+		$userInfo = $this->user->getInfo() ? $this->user->getInfo()->toArray(): null;
+		if(isset($userInfo['id'])) unset($userInfo['id']);
+		$data = array('email' => $this->user->getEmail());
+		if(is_array($userInfo)) $data = array_merge($data, $userInfo);
+		return $data;
 	}
 
 }
