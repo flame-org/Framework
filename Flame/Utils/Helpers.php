c0cb95ca0e8aa88d706a6f6e65c1259d3d898203
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-02 08:21:56 +0200

    MOved thumbnail creator to separated file

--- Flame/Utils/Helpers.php
+++ Flame/Utils/Helpers.php
@@ -2,7 +2,7 @@
 /**
  * Thumbnail
  *
- * @author  uestla
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
  * @package Flame
  *
  * @date    11.07.12
@@ -10,20 +10,14 @@
 
 namespace Flame\Utils;
 
-use \Nette\Image;
-
 class Helpers extends \Nette\Object
 {
 
-	private $thumbDirUri;
-	private $imageDirUri;
-	private $baseDir;
+	private $params;
 
 	public function __construct(array $params = array())
 	{
-		if(isset($params['baseDir'])) $this->baseDir = $params['baseDir'];
-		if(isset($params['thumbDir'])) $this->thumbDirUri = $params['thumbDir'];
-		if(isset($params['imageDir'])) $this->imageDirUri = $params['imageDir'];
+		$this->params = $params;
 	}
 
 	public function loader($helper)
@@ -39,108 +33,11 @@ class Helpers extends \Nette\Object
 		return $markDown->transformMarkdown($text);
 	}
 
-
-	/**
-     * Vytvoreni miniatury obrazku a vraceni jeho URI
-     *
-     * @param  string relativni URI originalu (zacina se v document_rootu)
-     * @param  NULL|int sirka miniatury
-     * @param  NULL|int vyska miniatury
-     * @return string absolutni URI miniatury
-     */
-    public function thumb($origName, $width, $height = NULL)
-    {
-
-	    if(is_null($this->baseDir) or is_null($this->imageDirUri) or is_null($this->thumbDirUri)){
-		    throw new \Nette\InvalidArgumentException;
-	    }
-
-        $thumbDirPath = $this->baseDir . '/' . trim($this->thumbDirUri, '/\\');
-        $origPath = $this->baseDir . '/' . $this->imageDirUri . '/' . $origName;
-
-	    $this->createDirForThumbnails($thumbDirPath);
-
-        if (($width === NULL && $height === NULL) || !is_file($origPath) || !is_dir($thumbDirPath) || !is_writable($thumbDirPath))
-            return $origName;
-
-        $thumbName = $this->getThumbName($origName, $width, $height, filemtime($origPath));
-        $thumbUri = trim($this->thumbDirUri, '/\\') . '/' . $thumbName;
-        $thumbPath = $thumbDirPath . '/' . $thumbName;
-
-	    // miniatura jiz existuje
-        if (is_file($thumbPath)) {
-            return $thumbUri;
-        }
-
-
-        try {
-
-            $image = Image::fromFile($origPath);
-
-            // zachovani pruhlednosti u PNG
-            $image->alphaBlending(FALSE);
-            $image->saveAlpha(TRUE);
-
-            $origWidth = $image->getWidth();
-            $origHeight = $image->getHeight();
-
-            $image->resize($width, $height,
-                $width !== NULL && $height !== NULL ? Image::STRETCH : Image::FIT)
-                ->sharpen();
-
-            $newWidth = $image->getWidth();
-            $newHeight = $image->getHeight();
-
-            // doslo ke zmenseni -> ulozime miniaturu
-            if ($newWidth !== $origWidth || $newHeight !== $origHeight) {
-
-                $image->save($thumbPath);
-
-                if (is_file($thumbPath))
-                    return $thumbUri;
-                else
-                    return $origName;
-
-            } else {
-                return $origName;
-            }
-
-        } catch (Exception $e) {
-            return $origName;
-        }
-    }
-
-
-
-    /**
-     * Vytvori jmeno generovane miniatury
-     *
-     * @param  string relativni cesta (document_root/$relPath)
-     * @param  int sirka
-     * @param  int vyska
-     * @param  int timestamp zmeny originalu
-     * @return string
-     */
-    private function getThumbName($relPath, $width, $height, $mtime)
-    {
-        $sep = '.';
-        $tmp = explode($sep, $relPath);
-        $ext = array_pop($tmp);
-
-        // cesta k obrazku (ale bez pripony)
-        $relPath = implode($sep, $tmp);
-
-        // pripojime rozmery a mtime
-        $relPath .= $width . 'x' . $height . '-' . $mtime;
-
-        // zahashujeme a vratime priponu
-        $relPath = md5($relPath) . $sep . $ext;
-
-        return $relPath;
-    }
-
-	private function createDirForThumbnails($filepath)
+	public function thumb($origName, $width, $height = NULL)
 	{
-		if(!file_exists($filepath)) return chmod($filepath, 0777, true);
+		$thumb = new ThumbnailsCreator($this->params);
+		return $thumb->thumb($origName, $width, $height);
 	}
+
+
 }
