0054228e8f5a1147029a3ab00f00e470e1733c1e
JSifalda <sifalda.jiri@gmail.com>
2012-08-19 18:27:51 +0200

    controls do not need be attached to presenter

--- Flame/Application/UI/Control.php
+++ Flame/Application/UI/Control.php
@@ -12,35 +12,5 @@ namespace Flame\Application\UI;
 
 abstract class Control extends \Nette\Application\UI\Control
 {
-	public function __construct(\Nette\ComponentModel\IContainer $parent, $name)
-	{
-		parent::__construct($parent, $name);
-	}
-
-	public function createTemplate($class = null)
-	{
-		$template = parent::createTemplate($class);
-		$template->registerHelperLoader(callback(
-			new \Flame\Templates\Helpers($this->getContextParameter('imageStorage')),
-			'loader'
-		));
-		return $template;
-	}
-
-	public function getSession()
-	{
-		return $this->getPresenter()->getSession();
-	}
-
-	private function getContext()
-	{
-		return $this->getPresenter()->getContext();
-	}
-
-	protected function getContextParameter($name = null)
-	{
-		$params = $this->getContext()->getParameters();
-		return (isset($params[$name])) ? $params[$name] : null;
-	}
 
 }
