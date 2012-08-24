284271d11d2207caaf4eed3cfff9e776c0d059b6
JSifalda <sifalda.jiri@gmail.com>
2012-08-24 16:44:39 +0200

    added IFactory for models

---
+++ Flame/Model/IFactory.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Factory.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    24.08.12
+ */
+
+namespace Flame\Model;
+
+interface IFactory
+{
+
+	const SAVE = true;
+
+	const NO_SAVE = false;
+
+}
