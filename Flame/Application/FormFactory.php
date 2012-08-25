22cf9622fce5abcf954d0a55b6ab51c173c489c0
JSifalda <sifalda.jiri@gmail.com>
2012-08-25 12:30:17 +0200

    added support for defaults data

--- Flame/Application/FormFactory.php
+++ Flame/Application/FormFactory.php
@@ -19,13 +19,21 @@ class FormFactory extends \Nette\Object
 	protected $form;
 
 	/**
+	 * @param array $defautls
 	 * @return \Nette\Application\UI\Form
 	 * @throws \Nette\InvalidStateException
 	 */
-	public function createForm()
+	public function createForm(array $defautls = array())
 	{
+
 		if(!$this->form instanceof \Nette\Application\UI\Form){
 			throw new \Nette\InvalidStateException;
+		}
+
+		if(count($defautls)){
+			$this->form->configureEdit($defautls);
+		}else{
+			$this->form->configureAdd();
 		}
 
 		return $this->form;
