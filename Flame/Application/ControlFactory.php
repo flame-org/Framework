54b979f302e0f2040615acd95feba93f20d7d908
JSifalda <sifalda.jiri@gmail.com>
2012-08-26 17:47:03 +0200

    added Control factory support

---
+++ Flame/Application/ControlFactory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ControlFactory.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    26.08.12
+ */
+
+namespace Flame\Application;
+
+abstract class ControlFactory extends \Nette\Object implements IControlFactory
+{
+
+}
---
+++ Flame/Application/IControlFactory.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * IControlFactory.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    26.08.12
+ */
+
+namespace Flame\Application;
+
+interface IControlFactory
+{
+
+	public function create();
+
+}
