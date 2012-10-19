f222a84b60be0b586763584f4045e615c9a54508
JSifalda <sifalda.jiri@gmail.com>
2012-10-19 22:50:05 +0200

    added default testCase for phpunit

---
+++ Flame/Tests/TestCase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TestCase.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    19.10.12
+ */
+
+namespace Flame\Tests;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+}
