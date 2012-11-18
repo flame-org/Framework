6763a81db90a5436239a87f177637485acba8e4c
JSifalda <sifalda.jiri@gmail.com>
2012-11-18 08:52:36 +0100

    Add support for @multiple creating of components

--- Flame/Application/UI/Presenter.php
+++ Flame/Application/UI/Presenter.php
@@ -52,4 +52,25 @@ abstract class Presenter extends \Nette\Application\UI\Presenter
 		return $this->getHttpRequest()->url->baseUrl;
 	}
 
+	/**
+	 * @param $name
+	 * @return \Nette\Application\UI\Multiplier|\Nette\ComponentModel\IComponent
+	 */
+	protected function createComponent($name)
+	{
+		$method = 'createComponent' . ucfirst($name);
+		if (method_exists($this, $method) && \Nette\Reflection\Method::from($this, $method)->hasAnnotation('multiple')) {
+			$presenter = $this;
+			return new \Nette\Application\UI\Multiplier(function ($id) use ($presenter, $method) {
+				$defaultArgs = array($presenter, $id);
+				return call_user_func_array(array($presenter, $method), $defaultArgs);
+			});
+			# in PHP 5.4 factory for multiplied component can be protected
+			# return new UI\Multiplier(function ($id) use ($name) {
+			#	return $this->$method($this, $id, $this->getDataset($name));
+			# });
+		}
+		return parent::createComponent($name);
+	}
+
 }
