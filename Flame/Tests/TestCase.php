d2389514455069570a88d2b5d73716b99b5c0d87
JSifalda <sifalda.jiri@gmail.com>
2012-11-18 15:58:55 +0100

    Refactoring, add methods for testing UI\Form

--- Flame/Tests/TestCase.php
+++ Flame/Tests/TestCase.php
@@ -10,23 +10,91 @@
 
 namespace Flame\Tests;
 
+use Nette\Application\UI\Form;
+
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
 
+	/** @var \Flame\Tests\Configurator */
+	private $context;
+
 	/**
-	 * @return \Nette\DI\Container
+	 * @param null $name
+	 * @param array $data
+	 * @param string $dataName
 	 */
-	public function getContainer()
+	public function __construct($name = null, array $data = array(), $dataName = '')
 	{
-		return Environment::getService('container');
+		new \Flame\Tests\Configurator;
+		$this->context = Configurator::getTestsContainer();
+
+		parent::__construct($name, $data, $dataName);
 	}
 
 	/**
-	 * @return mixed
+	 * @return Configurator
 	 */
 	public function getContext()
 	{
-		return Environment::getContext();
+		return $this->context;
+	}
+
+	/********************* Nette Forms *********************/
+
+
+	/**
+	 * @param \Nette\Application\UI\Form $form
+	 * @param array $values
+	 * @return \Nette\Application\IResponse
+	 */
+	public function submitForm(Form $form, array $values = array())
+	{
+		$get = $form->getMethod() !== Form::POST ? $values : array();
+		$post = $form->getMethod() === Form::POST ? $values : array();
+		list($post, $files) = $this->separateFilesFromPost($post);
+
+		$presenter = new Tools\UIFormTestingPresenter($form);
+
+		$this->getContext()->callMethod(array($presenter, 'injectPrimary'));
+		return $presenter->run(new \Nette\Application\Request(
+			'presenter',
+			strtoupper($form->getMethod()),
+			array('do' => 'form-submit', 'action' => 'default') + $get,
+			$post,
+			$files
+		));
+	}
+
+
+
+	/**
+	 * @param array $post
+	 * @param array $files
+	 *
+	 * @return array
+	 */
+	private function separateFilesFromPost(array $post, array $files = array())
+	{
+		foreach ($post as $key => $value) {
+			if (is_array($value)) {
+				list($pPost, $pFiles) = $this->separateFilesFromPost($value);
+				unset($post[$key]);
+
+				if ($pPost) {
+					$post[$key] = $pPost;
+				}
+				if ($pFiles) {
+					$files[$key] = $pFiles;
+				}
+			}
+
+			if ($value instanceof \Nette\Http\FileUpload) {
+				$files[$key] = $value;
+				unset($post[$key]);
+			}
+		}
+
+		return array($post, $files);
 	}
 
 }
