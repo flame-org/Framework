46cbc27ad1833517820c6c2498f0efa1e4a45bea
JSifalda <sifalda.jiri@gmail.com>
2012-11-24 19:19:13 +0100

    Add method for setting debug mode

--- Flame/Config/Configurator.php
+++ Flame/Config/Configurator.php
@@ -38,4 +38,19 @@ class Configurator extends \Nette\Config\Configurator
 		$this->parameters['consoleMode'] = $name === 'console' ?: PHP_SAPI === 'cli';
 		return $this;
 	}
+
+	/**
+	 * When given NULL, the debug mode gets detected automatically
+	 *
+	 * @param null $value
+	 * @return Configurator
+	 */
+	public function setDebugMode($value = null)
+	{
+		$this->parameters['debugMode'] = is_bool($value) ? $value
+			: \Nette\Config\Configurator::detectDebugMode($value);
+		$this->parameters['productionMode'] = !$this->parameters['debugMode'];
+		$this->parameters['kdyby']['debug'] = $this->parameters['debugMode'];
+		return $this;
+	}
 }
