98d832a684e1676520e34ad3405be97a4a73dd61
JSifalda <sifalda.jiri@gmail.com>
2012-11-29 18:26:02 +0100

    Remove unncessary methods for tests

--- Flame/Config/Configurator.php
+++ Flame/Config/Configurator.php
@@ -27,30 +27,4 @@ class Configurator extends \Nette\Config\Configurator
 			)
 		));
 	}
-
-	/**
-	 * @param $name
-	 * @return Configurator
-	 */
-	public function setEnvironment($name)
-	{
-		$this->parameters['environment'] = $name;
-		$this->parameters['consoleMode'] = $name === 'console' ?: PHP_SAPI === 'cli';
-		return $this;
-	}
-
-	/**
-	 * When given NULL, the debug mode gets detected automatically
-	 *
-	 * @param null $value
-	 * @return Configurator
-	 */
-	public function setDebugMode($value = null)
-	{
-		$this->parameters['debugMode'] = is_bool($value) ? $value
-			: \Nette\Config\Configurator::detectDebugMode($value);
-		$this->parameters['productionMode'] = !$this->parameters['debugMode'];
-		$this->parameters['kdyby']['debug'] = $this->parameters['debugMode'];
-		return $this;
-	}
 }
