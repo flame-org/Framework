42f186efb2dc526bdcec389d4663f17932484f9d
JSifalda <sifalda.jiri@gmail.com>
2012-12-22 11:07:02 +0100

    Refactoring, add registration of helpers

--- Flame/Config/Extensions/NetteExtension.php
+++ Flame/Config/Extensions/NetteExtension.php
@@ -10,12 +10,15 @@
 
 namespace Flame\Config\Extensions;
 
+use Nette;
+
 class NetteExtension extends \Nette\Config\Extensions\NetteExtension
 {
 
 	public $helpersDefauls = array(
 		'template' => array(
 			'helperLoaders' => '\Nette\Templating\Helpers',
+			'helpers' => array(),
 		)
 	);
 
@@ -25,23 +28,15 @@ class NetteExtension extends \Nette\Config\Extensions\NetteExtension
 
 		$container = $this->getContainerBuilder();
 		$config = $this->getConfig($this->helpersDefauls);
+		$this->compiler->parseServices($container, array());
 
 		$this->setupTemplating($container, $config);
 	}
 
 	private function setupTemplating(\Nette\DI\ContainerBuilder $container, array $config)
 	{
-		/** @var $latte \Nette\Latte\Engine */
+
 		$latte = $container->getDefinition($this->prefix('latte'));
-
-
-		//Prepared for configuration of helpers
-//		if(isset($config['template']['helpers']) and count($config['template']['helpers'])){
-//			foreach($config['template']['helpers'] as $helper){
-//				$latte->addSetup($helper . '(?->compiler)', array('@self'));
-//			}
-//		}
-
 		$helperLoaders = $config['template']['helperLoaders'];
 
 		if (strpos($helperLoaders, '::') === false) {
@@ -49,12 +44,49 @@ class NetteExtension extends \Nette\Config\Extensions\NetteExtension
 		}
 
 		$container->removeDefinition($this->prefix('template'));
-		$container->addDefinition($this->prefix('template'))
-			->setClass('Nette\Templating\FileTemplate')
+		$template = $container->addDefinition($this->prefix('template'))
+			->setClass('Nette\Templating\ITemplate')
+			->setFactory('? ? new ? : new Nette\Templating\FileTemplate', array('%class%', new Nette\PhpGenerator\PhpLiteral('?'), '%class%'))
+			->setParameters(array('class' => null))
+			->setImplement('Flame\Templating\ITemplateFactory')
 			->addSetup('registerFilter', array($latte))
 			->addSetup('registerHelperLoader', array($helperLoaders))
-			->setShared(false);
+			->addSetup('setCacheStorage', array($this->prefix('@templateCacheStorage')))
+			->addSetup('$service->presenter = $service->_presenter = ?', array(new Nette\DI\Statement('@application::getPresenter')))
+			->addSetup('$service->control = $service->_control = ?', array(new Nette\DI\Statement('@application::getPresenter')))
+			->addSetup('$user', array('@user'))
+			->addSetup('$netteHttpResponse', array('@httpResponse'))
+			->addSetup('$netteCacheStorage', array('@cacheStorage'))
+			->addSetup('$service->baseUri = $service->baseUrl = rtrim(?->getBaseUrl(), "/")', array(new Nette\DI\Statement('@httpRequest::getUrl')))
+			->addSetup('$service->basePath = preg_replace(?, "", $service->baseUrl)', array('#https?://[^/]+#A'))
+			->setAutowired(true);
 
+		if(count($config['template']['helpers'])){
+			foreach($config['template']['helpers'] as $helper){
+				$attributes = $this->expandAttributes($container, $helper['class']->{'attributes'});
+				$helperService = new \ReflectionClass($helper['class']->{'value'});
+				$helperService = $helperService->newInstanceArgs($attributes);
+				$template->addSetup('registerHelper', array($helper['method'], array($helperService, $helper['method'])));
+			}
+		}
+
+	}
+
+	/**
+	 * @param \Nette\DI\ContainerBuilder $container
+	 * @param $atributes
+	 * @return array
+	 */
+	private function expandAttributes(\Nette\DI\ContainerBuilder $container, $atributes)
+	{
+		$prepared = array();
+		if(count($atributes)){
+			foreach($atributes as $atribute){
+				$prepared[] = $container->expand($atribute);
+			}
+		}
+
+		return $prepared;
 	}
 
 }
