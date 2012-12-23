07e714b92719a402b54d3bbe5ad58687f6e0dd08
JSifalda <sifalda.jiri@gmail.com>
2012-12-23 23:30:47 +0100

    Remove unnecessary constructor

--- Flame/Model/Repository.php
+++ Flame/Model/Repository.php
@@ -14,15 +14,6 @@ class Repository extends \Doctrine\ORM\EntityRepository implements IRepository
 {
 
 	/**
-	 * @param \Doctrine\ORM\EntityManager $em
-	 * @param \Doctrine\ORM\Mapping\ClassMetadata $class
-	 */
-	public function __construct(\Doctrine\ORM\EntityManager $em, \Doctrine\ORM\Mapping\ClassMetadata $class)
-	{
-		parent::__construct($em, $class);
-	}
-
-	/**
 	 * @param $entity
 	 * @param bool $withoutFlush
 	 * @return Repository
