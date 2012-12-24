53fc7d5fe081396664ea0b20393edc8d4ca59133
JSifalda <sifalda.jiri@gmail.com>
2012-12-24 12:10:05 +0100

    Add test of repository

---
+++ tests/Flame/Model/RepositoryTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * RepositoryTest.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package
+ *
+ * @date    24.12.12
+ */
+
+namespace Flame\Tests\Model;
+
+class RepositoryTest extends \Flame\Tests\DoctrineTestCase
+{
+
+	public function testProperties()
+	{
+		$emMock = $this->getEmMock();
+		$classMetadataMock = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+			->disableOriginalConstructor()
+			->getMock();
+		$repository = new \Flame\Model\Repository($emMock, $classMetadataMock);
+
+		$this->assertAttributeEquals($emMock, '_em', $repository);
+		$this->assertAttributeEquals($classMetadataMock, '_class', $repository);
+	}
+
+	public function testDeleteWithoutFlush()
+	{
+		//TODO: Fix: Call to a member function remove() on a non-object
+//		$entityMock = $this->getMock('Flame\Doctrine\Entity');
+//		$emMock = $this->getEmMock();
+//		$emMock->expects($this->once())
+//			->method('remove')
+//			->with($entityMock)
+//			->will($this->returnValue(null));
+//		$classMetadataMock = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadata')
+//			->disableOriginalConstructor()
+//			->getMock();
+//		$repository = new \Flame\Model\Repository($emMock, $classMetadataMock);
+//		$repository->delete($entityMock, true);
+	}
+
+}
