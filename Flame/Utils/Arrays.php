85047d11ba177d1a35b6b83b0170d46d62992e00
JSifalda <sifalda.jiri@gmail.com>
2012-12-25 23:13:59 +0100

    Add Arrays class

---
+++ Flame/Utils/Arrays.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Arrays.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    25.12.12
+ */
+
+namespace Flame\Utils;
+
+use Flame;
+
+final class Arrays extends \Nette\Object
+{
+
+	/**
+	 * Static class - cannot be instantiated.
+	 *
+	 * @throws \Flame\StaticClassException
+	 */
+	final public function __construct()
+	{
+		throw new Flame\StaticClassException;
+	}
+
+	/**
+	 * Mirror of Nette\Utils\Arrays
+	 *
+	 * @param string $name
+	 * @param array $args
+	 * @return mixed
+	 */
+	public static function __callStatic($name, $args)
+	{
+		return callback('Nette\Utils\Arrays', $name)->invokeArgs($args);
+	}
+
+	/**
+	 * @param $array
+	 * @param $subkey
+	 * @param int $sortType
+	 * @return mixed
+	 */
+	public static function sortBySubkey(&$array, $subkey, $sortType = SORT_ASC) {
+		foreach ($array as $subarray) {
+			$keys[] = $subarray->$subkey;
+		}
+
+		array_multisort($keys, $sortType, $array);
+		return $array;
+	}
+}
