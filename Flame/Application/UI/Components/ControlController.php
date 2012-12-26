76819773c90009c602f28679e68fe911bf12d217
JSifalda <sifalda.jiri@gmail.com>
2012-12-26 12:04:45 +0100

    Add ControlController

---
+++ Flame/Application/UI/Components/ControlController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * ControlController.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame
+ *
+ * @date    26.12.12
+ */
+
+namespace Flame\Application\UI\Components;
+
+abstract class ControlController extends \Nette\Object implements IComponentController
+{
+
+}
