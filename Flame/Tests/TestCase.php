52b01937cb7bc845a9cb5b127b6a59a56be9a931
JSifalda <sifalda.jiri@gmail.com>
2013-01-02 23:37:42 +0100

    Add Nette\Object behaviour

--- Flame/Tests/TestCase.php
+++ Flame/Tests/TestCase.php
@@ -10,6 +10,8 @@
 
 namespace Flame\Tests;
 
+use Nette\ObjectMixin;
+
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
 
@@ -44,5 +46,60 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
 		return $method;
 	}
 
+	/********************* Nette\Object behaviour ****************d*g**/
+	/**
+	 * @return \Nette\Reflection\ClassType
+	 */
+	public static function getReflection()
+	{
+		return new \Nette\Reflection\ClassType(get_called_class());
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return mixed
+	 */
+	public function &__get($name)
+	{
+		return ObjectMixin::get($this, $name);
+	}
+
+	/**
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	public function __set($name, $value)
+	{
+		ObjectMixin::set($this, $name, $value);
+	}
+
+	/**
+	 * @param string $name
+	 * @param array $args
+	 * @return mixed
+	 */
+	public function __call($name, $args)
+	{
+		return ObjectMixin::call($this, $name, $args);
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public function __isset($name)
+	{
+		return ObjectMixin::has($this, $name);
+	}
+
+	/**
+	 * @param string $name
+	 */
+	public function __unset($name)
+	{
+		ObjectMixin::remove($this, $name);
+	}
 
 }
