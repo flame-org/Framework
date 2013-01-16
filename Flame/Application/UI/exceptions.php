f72bd19302f5010357959373a9efe19185fdaf27
JSifalda <sifalda.jiri@gmail.com>
2013-01-16 17:06:17 +0100

    Add \Flame\Application\UI exceptions

---
+++ Flame/Application/UI/exceptions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * exceptions.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date    16.01.13
+ */
+
+namespace Flame\Application\UI;
+
+/**
+ * The exception that is thrown when error is occured during form processing
+ */
+class FormProcessException extends \Nette\InvalidArgumentException
+{
+
+}
