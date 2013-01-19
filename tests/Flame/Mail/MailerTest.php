81b4489fc12e79263476ec0b32c4351c405fe1a3
JSifalda <sifalda.jiri@gmail.com>
2013-01-19 22:50:04 +0100

    Add test of Mailer

---
+++ tests/Flame/Mail/MailerTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * MailerTest.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date    19.01.13
+ */
+
+namespace Flame\Tests\Mail;
+
+class MailerTest extends \Flame\Tests\TestCase
+{
+
+	/**
+	 * @var \Flame\Mail\Mailer
+	 */
+	private $mailer;
+
+	public function setUp()
+	{
+		$this->mailer = $this->getMockForAbstractClass('\Flame\Mail\Mailer');
+	}
+
+	public function testCreateMessage()
+	{
+		$this->assertInstanceOf('\Nette\Mail\Message', $this->mailer->createMessage());
+	}
+
+	public function testGetFileTemplate()
+	{
+		$fileTemplateMock = $this->getMock('\Nette\Templating\FileTemplate');
+		$fileTemplateMock->expects($this->once())
+			->method('registerFilter');
+		$fileTemplateMock->expects($this->once())
+			->method('registerHelperLoader')
+			->with($this->equalTo('registerHelperLoader'));
+		$this->mailer->injectFileTemplate($fileTemplateMock);
+		
+		$this->assertInstanceOf('\Nette\Templating\FileTemplate', $this->mailer->getFileTemplate());
+	}
+
+}
