cbdbfe7370bbe21fa564b43e18566fb5837fa45b
JSifalda <sifalda.jiri@gmail.com>
2013-01-19 15:33:35 +0100

    Add basic mailer

---
+++ Flame/Mail/Mailer.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Mailer.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date    19.01.13
+ */
+
+namespace Flame\Mail;
+
+abstract class Mailer extends \Nette\Object
+{
+
+	/**
+	 * @var \Nette\Templating\FileTemplate $fileTemplate
+	 */
+	private $fileTemplate;
+
+	/**
+	 * @param \Nette\Templating\FileTemplate $fileTemplate
+	 */
+	public function injectFileTemplate(\Nette\Templating\FileTemplate $fileTemplate)
+	{
+		$this->fileTemplate = $fileTemplate;
+	}
+
+	/**
+	 * @return \Nette\Templating\FileTemplate
+	 */
+	public function getFileTemplate()
+	{
+		$this->fileTemplate->registerFilter(\Nette\Callback::create(new \Nette\Latte\Engine()));
+		$this->fileTemplate->registerHelperLoader('Nette\Templating\Helpers::loader');
+		$fileTemplate = clone $this->fileTemplate;
+		return $fileTemplate;
+	}
+
+//	/**
+//	 * @param \Nette\Mail\Message $message
+//	 * @return bool
+//	 */
+//	public function send(\Nette\Mail\Message $message)
+//	{
+//		try{
+//			$this->mailer->send($message);
+//			return true;
+//		}catch (\Exception $e){
+//			\Nette\Diagnostics\Debugger::log($e);
+//			return false;
+//		}
+//	}
+
+	/**
+	 * @return \Nette\Mail\Message
+	 */
+	public function createMessage()
+	{
+		return new \Nette\Mail\Message;
+	}
+
+}
