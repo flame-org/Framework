6d2305771ae7571aa0ee0a57f96187c33203ab90
JSifalda <sifalda.jiri@gmail.com>
2013-01-23 16:21:38 +0100

    Construct require array only

--- Flame/Database/Connection.php
+++ Flame/Database/Connection.php
@@ -17,6 +17,34 @@ class Connection extends \Nette\Database\Connection
 	private $selectionFactory;
 
 	/**
+	 * @var array
+	 */
+	private $options = array(
+		'driver' => null,
+		'host' => null,
+		'dbname' => null,
+		'user' => null,
+		'password' => null,
+		'options' => array(),
+		'driver_class' => null
+	);
+
+	/**
+	 * @param array $options
+	 */
+	public function __construct(array $options)
+	{
+		$this->options = array_merge($this->options, $options);
+		parent::__construct(
+			$this->createDns(),
+			$this->options['user'],
+			$this->options['password'],
+			$this->options['options'],
+			$this->options['driver_class']
+		);
+	}
+
+	/**
 	 * Creates selector for table.
 	 * @param  string
 	 * @return \Nette\Database\Table\Selection
@@ -27,6 +55,14 @@ class Connection extends \Nette\Database\Connection
 			$this->selectionFactory = new Table\SelectionFactory($this);
 		}
 		return $this->selectionFactory->create($table);
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function createDns()
+	{
+		return $this->options['driver'] . ':host=' . $this->options['host'] . ';dbname=' . $this->options['dbname'];
 	}
 
 }
