7e0a5be660a0c59bb420422199ccbfa5b6941f3f
JSifalda <sifalda.jiri@gmail.com>
2013-01-26 00:08:20 +0100

    Add tests of getTableClassParameters and setTableProperties

--- tests/Flame/Database/Table/SelectionTest.php
+++ tests/Flame/Database/Table/SelectionTest.php
@@ -11,6 +11,24 @@ namespace Flame\Tests\Database\Table;
 class FakeTable extends \Flame\Database\Table
 {
 
+	protected $name;
+
+	protected $surname;
+
+	public function __construct($name)
+	{
+		$this->name = (string) $name;
+	}
+
+	public function setName($name)
+	{
+		$this->name = (string) $name;
+	}
+
+	public function setSurname($surname)
+	{
+		$this->surname = (string) $surname;
+	}
 }
 
 class SelectionTest extends \Flame\Tests\TestCase
@@ -43,7 +61,37 @@ class SelectionTest extends \Flame\Tests\TestCase
 			$method->invoke($failSelection, array('id' => 1));
 			$this->fail('Expected exception ReflectionException');
 		}catch (\ReflectionException $ex){}
+	}
 
+	public function testSetTableProperties()
+	{
+		$row = array(
+			'id' => 1,
+			'name' => 'George',
+			'Surname' => 'Good'
+		);
+
+		$fakeTable = new FakeTable($row['name']);
+
+		$method = $this->getAccessibleMethod('\Flame\Database\Table\Selection', 'setTableProperties');
+		$r = $method->invoke($this->selection, $row, $fakeTable);
+		$this->assertInstanceOf('\Flame\Database\Table', $r);
+		$this->assertAttributeEquals(1, 'id', $r);
+	}
+
+	public function testGetTableClassParameters()
+	{
+		$row = array(
+			'id' => 1,
+			'name' => 'George',
+			'Surname' => 'Good'
+		);
+
+		$reflection = new \ReflectionClass('\Flame\Tests\Database\Table\FakeTable');
+
+		$method = $this->getAccessibleMethod('\Flame\Database\Table\Selection', 'getTableClassParameters');
+		$r = $method->invoke($this->selection, $row, $reflection);
+		$this->assertEquals(array('George'), $r);
 	}
 
 }
