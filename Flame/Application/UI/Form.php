a93f1ca2158db5f83759d04de49b6ce10eab0bde
JSifalda <sifalda.jiri@gmail.com>
2013-04-17 16:58:43 +0200

    Remove fireEvents improvements

--- Flame/Application/UI/Form.php
+++ Flame/Application/UI/Form.php
@@ -68,34 +68,6 @@ class Form extends \Nette\Application\UI\Form
 	}
 
 	/**
-	 * Fires send/click events.
-	 * @author Filip Procházka (filip.prochazka@kdyby.org)
-	 * @return void
-	 */
-	public function fireEvents()
-	{
-		if (!$this->isSubmitted()) {
-			return;
-
-		} elseif ($this->isSubmitted() instanceof ISubmitterControl) {
-			if (!$this->isSubmitted()->getValidationScope() || $this->isValid()) {
-				$this->dispatchEvent($this->isSubmitted()->onClick, $this->isSubmitted());
-				$valid = true;
-
-			} else {
-				$this->dispatchEvent($this->isSubmitted()->onInvalidClick, $this->isSubmitted());
-			}
-		}
-
-		if (isset($valid) || $this->isValid()) {
-			$this->dispatchEvent($this->onSuccess, $this);
-
-		} else {
-			$this->dispatchEvent($this->onError, $this);
-		}
-	}
-
-	/**
 	 * @param array $defaults
 	 */
 	public function restore(array $defaults = array())
@@ -135,37 +107,6 @@ class Form extends \Nette\Application\UI\Form
 		}
 
 		return parent::setDefaults($values, $erase);
-	}
-
-	/**
-	 * @author Filip Procházka (filip.prochazka@kdyby.org)
-	 * @param array|\Traversable $listeners
-	 * @param mixed $arg
-	 */
-	protected function dispatchEvent($listeners, $arg = null)
-	{
-		$args = func_get_args();
-		$listeners = array_shift($args);
-
-        if(class_exists('\Kdyby\Events\Event') && $listeners instanceof \Kdyby\Events\Event)
-            $listeners = $listeners->getListeners();
-
-		foreach ((array)$listeners as $handler) {
-			if ($handler instanceof \Nette\Application\UI\Link) {
-				if (!$this->isValid()) continue;
-				/** @var \Nette\Application\UI\Link $handler */
-				$refl = $handler->getReflection();
-				/** @var \Nette\Reflection\ClassType $refl */
-				$compRefl = $refl->getProperty('component');
-				$compRefl->accessible = true;
-				/** @var \Nette\Application\UI\PresenterComponent $component */
-				$component = $compRefl->getValue($handler);
-				$component->redirect($handler->getDestination(), $handler->getParameters());
-
-			} else {
-				callback($handler)->invokeArgs($args);
-			}
-		}
 	}
 
 	/**
