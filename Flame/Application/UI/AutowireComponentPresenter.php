d9ad4d454f0760da0e50fb8b72fd4ad1bc9327c7
JSifalda <sifalda.jiri@gmail.com>
2013-04-27 09:55:39 +0200

    Exclude & improve autowire of components factories

--- Flame/Application/UI/AutowireComponentPresenter.php
+++ Flame/Application/UI/AutowireComponentPresenter.php
@@ -7,7 +7,69 @@
  */
 namespace Flame\Application\UI;
 
-class AutowireComponentPresenter
+use Nette;
+
+/**
+ * @author matej21
+ */
+class AutowireComponentPresenter extends Presenter
 {
+
+	/** @var Nette\DI\Container */
+	private $serviceLocatorForFactories;
+
+	/**
+	 * @return Nette\DI\Container
+	 */
+	protected function getServiceLocatorForFactories()
+	{
+		if ($this->serviceLocatorForFactories === NULL) {
+			return $this->getPresenter()->getContext(); // fallback
+		}
+
+		return $this->serviceLocatorForFactories;
+	}
+
+	/**
+	 * @param \Nette\DI\Container $sl
+	 * @internal
+	 */
+	public function injectServiceLocatorForFactories(Nette\DI\Container $sl)
+	{
+		$this->serviceLocatorForFactories = $sl;
+	}
+
+	/**
+	 * @param $name
+	 * @return Nette\ComponentModel\IComponent
+	 * @throws Nette\UnexpectedValueException
+	 */
+	public function createComponent($name)
+	{
+		$sl = $this->getServiceLocatorForFactories();
+		$ucname = ucfirst($name);
+		$method = 'createComponent' . $ucname;
+		if ($ucname !== $name && method_exists($this, $method)) {
+			$reflection = $this->getReflection()->getMethod($method);
+			if($reflection->getName() !== $method) {
+				return;
+			}
+			$parameters = $reflection->parameters;
+
+			$args = array();
+			if (($first = reset($parameters)) && !$first->className) {
+				$args[] = $name;
+			}
+
+			$args = Nette\DI\Helpers::autowireArguments($reflection, $args, $sl);
+			$component = call_user_func_array(array($this, $method), $args);
+			if (!$component instanceof Nette\ComponentModel\IComponent && !isset($this->components[$name])) {
+				throw new Nette\UnexpectedValueException("Method $reflection did not return or create the desired component.");
+			}
+
+			return $component;
+		}
+
+	}
 
 }
--- Flame/Application/UI/Presenter.php
+++ Flame/Application/UI/Presenter.php
@@ -48,36 +48,4 @@ abstract class Presenter extends \Nette\Application\UI\Presenter
 		return $this->getHttpRequest()->url->baseUrl;
 	}
 
-	/**
-	 * @param $name
-	 * @return \Nette\ComponentModel\IComponent
-	 * @throws \Nette\UnexpectedValueException
-	 */
-	protected function createComponent($name)
-	{
-		$ucname = ucfirst($name);
-		$method = 'createComponent' . $ucname;
-		if ($ucname !== $name && method_exists($this, $method)) {
-			$reflection = $this->getReflection()->getMethod($method);
-			if ($reflection->getName() !== $method) {
-				return;
-			}
-			$class = get_class($this);
-			$parameters = $reflection->parameters;
-
-			$args = array();
-			if (isset($parameters[0]) and !$parameters[0]->className) {
-				$args[] = $name;
-			}
-
-			$args = \Nette\DI\Helpers::autowireArguments($reflection, $args, $this->context);
-			$component = call_user_func_array(array($this, $method), $args);
-			if (!$component instanceof \Nette\ComponentModel\IComponent && !isset($this->components[$name])) {
-				throw new \Nette\UnexpectedValueException("Method $class::$method() did not return or create the desired component.");
-			}
-
-			return $component;
-		}
-	}
-
 }
