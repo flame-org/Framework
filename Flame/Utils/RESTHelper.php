ad86de326fcd6c5578a9951d9d442faf0258515c
JSifalda <sifalda.jiri@gmail.com>
2013-05-12 19:50:30 +0200

    Add REST validator

---
+++ Flame/Utils/RESTHelper.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Class RESTHelper
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 12.05.13
+ */
+namespace Flame\Utils;
+
+use Flame\StaticClassException;
+use Nette\InvalidArgumentException;
+use Nette\Object;
+use Nette\Reflection\ClassType;
+
+class RESTHelper extends Object
+{
+
+	/**
+	 * @throws StaticClassException
+	 */
+	final public function __construct()
+	{
+		throw new StaticClassException;
+	}
+
+	/**
+	 * @param      $var
+	 * @param bool $getPrivate
+	 * @return array
+	 * @throws \Nette\InvalidArgumentException
+	 */
+	public static function invalidateObject($var, $getPrivate = true)
+	{
+		if(is_object($var)) {
+
+			if($getPrivate) {
+				/** @var  $valid */
+				$valid = array();
+				$vars = ClassType::from($var)->getProperties();
+				if(count($vars)) {
+					/** @var \Nette\Reflection\Property $property */
+					foreach($vars as $property) {
+						$property->setAccessible(true);
+						$value = $property->getValue($var);
+						if($value instanceof \DateTime){
+							$valid[$property->getName()] = $value->getTimestamp();
+						}elseif(!is_object($value)) {
+							$valid[$property->getName()] = $value;
+						}
+					}
+				}
+
+				return $valid;
+			}else{
+				return get_object_vars($var);
+			}
+		}else{
+			throw new InvalidArgumentException('Parameter must be object. Given ' . gettype($var));
+		}
+	}
+
+}
