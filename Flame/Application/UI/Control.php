70a906417eaff3e70821f9d423707f2ad7737409
JSifalda <sifalda.jiri@gmail.com>
2013-05-18 17:41:09 +0200

    Remove hooks & views support

--- Flame/Application/UI/Control.php
+++ Flame/Application/UI/Control.php
@@ -18,35 +18,7 @@ use Nette\Callback;
 abstract class Control extends \Nette\Application\UI\Control
 {
 
-	/** @var array */
-	public $onBeforeRender = array();
-
-	/**
-	 * @return mixed
-	 * @throws \Nette\InvalidStateException
-	 */
 	public function render()
-	{
-		$params = func_get_args();
-		$type = array_shift($params);
-		$method = ($type !== null) ? 'render' . ucfirst($type) : 'renderDefault';
-
-		if (count($this->onBeforeRender)) {
-			foreach ($this->onBeforeRender as $callback) {
-				if ($callback instanceof Callback) {
-					$callback->invokeArgs($params);
-				} elseif (is_array($callback) && count($callback)) {
-					Callback::create($callback)->invokeArgs($params);
-				} else {
-					throw new \Nette\InvalidStateException('Invalid before render hook callback');
-				}
-			}
-		}
-
-		return Callback::create($this, $method)->invokeArgs($params);
-	}
-
-	public function renderDefault()
 	{
 		$this->template->render();
 	}
