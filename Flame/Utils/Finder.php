148a81656b933084998d4d0ce0d6328345030cf0
JSifalda <sifalda.jiri@gmail.com>
2013-07-09 22:54:26 +0200

    Add finder, ordering methods

---
+++ Flame/Utils/Finder.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Class Finder
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 09.07.13
+ */
+namespace Flame\Utils;
+
+class Finder extends \Nette\Utils\Finder
+{
+
+	/** @var  \Closure */
+	private $order;
+
+	/**
+	 * Sets the order comparison function
+	 * @param callback $order
+	 * @return Finder
+	 */
+	public function order(\Closure $order)
+	{
+		$this->order = $order;
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	public function orderByName()
+	{
+		$this->order = function($f1, $f2) {
+			return strcasecmp($f1->getFilename(), $f2->getFilename());
+		};
+		return $this;
+	}
+
+	/**
+	 * @return $this
+	 */
+	public function orderByMTime()
+	{
+		$this->order = function($f1, $f2) {
+			return $f2->getMTime() - $f2->getMTime();
+		};
+		return $this;
+	}
+
+
+	/**
+	 * Returns iterator.
+	 * @return \Iterator
+	 */
+	public function getIterator()
+	{
+		$iterator = parent::getIterator();
+		if ($this->order === NULL) {
+			return $iterator;
+		}
+
+		$iterator = new \ArrayIterator(iterator_to_array($iterator));
+		$iterator->uasort($this->order);
+
+		return $iterator;
+	}
+
+}
